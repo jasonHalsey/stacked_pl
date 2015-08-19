dda27aee7f8d636adad2113561c6d725d646b101
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2015-08-18 17:09:29 -0700

    remove duplicate typekit script

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <div id="top"></div>
    <section id="video" class="video-content-stripe">
         <div class="header-video">
-            <img src="images/video-fallback.jpg" class="header-video--media" data-video-src="135496966"  data-provider="Vimeo" data-video-width="500" data-video-height="281">
-            <a href="https://vimeo.com/135496966" class="video-trigger" id="video-trigger">Play video</a>
+            <img src="images/video-fallback.jpg" class="header-video--media" data-video-src="136325708"  data-provider="Vimeo" data-video-width="500" data-video-height="281">
+            <a href="https://vimeo.com/136325708" class="video-trigger" id="video-trigger">Play video</a>
         </div>
         
     </section>
