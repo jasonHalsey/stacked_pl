112ba27a865b587b3c72f881caec6ea40af810a9
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2015-10-16 08:03:42 -0700

    add new images and layout

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 
     <div class='embed-container'>
       <img class="vimeoplayer" data-vimeoid="136325708" src="images/video-fallback.jpg"  alt="Powered By Stacked">
-      <div class="video-trigger" id="video-trigger">Watch Video</div>
+      <!-- <div class="video-trigger" id="video-trigger">Watch Video</div> -->
     </div>
 
 
     <div class="header delayed nonfixed">
       
-        <a href="#mailchimp" class="signup desktop" id="list-trigger">Pre-Order Mailing List</a>
+        <a href="#mailchimp" class="signup desktop" id="list-trigger">Pre-Order<br />Mailing List</a>
         <img src="images/logo.svg" />
         <a href="#mailchimp" class="signup mobile" id="list-trigger">Pre-Order Mailing List</a>
 
@@ -20,7 +20,7 @@
 
     <div class="header delayed fixed">
       
-        <a href="#mailchimp" class="signup desktop" id="list-trigger">Pre-Order Mailing List</a>
+        <a href="#mailchimp" class="signup desktop" id="list-trigger">Pre-Order<br />Mailing List</a>
         <img src="images/logo.svg" />
         <a href="#mailchimp" class="signup mobile" id="list-trigger">Pre-Order Mailing List</a>
 
@@ -30,8 +30,11 @@
     <div id="main" class="delayed">
       <div class="overlay-container">
         <section id="block_1" class="content-stripe story-container">
-            <div class="content-block_left">
-                <h1>wireless charging</h1>
+            <div class=" ">
+                <h1><span>TRUE</span>wireless charging</h1>
+
+                <img src="images/copy-blocks/02.jpg" />
+
                 <p>
                     Wireless charging just took a giant step forward. STACKED charges faster than competing technologies because our magnetic connectors form direct contact to charge your iPhone®.  
                 </p>
@@ -39,27 +42,28 @@
                 <p>
                     STACKED is the first wireless charging ecosystem to charge your iPhone® just as efficiently as plugging into the wall.
                 </p>
-            </div>
-            <div class="img-block_right">
-                <img src="images/copy-blocks/stacked_5.jpg" />
+          
+                
             </div>
         </section>
         
 
         <section id="block_2" class="content-stripe story-container">
-            <div class="content-block_right">
+            <div class=" ">
                 <h1>magnetic connection</h1>
+
+                <img src="images/copy-blocks/06.jpg" />
+
                 <p>
                     At the heart of the Stacked iPhone® case is our magnetic connector which is built directly into the back of all our slim cases to allow easy wireless charging while offering full protection. Direct battery transfer works even more efficiently than plugging into the wall.
                 </p>
-            </div>
-            <div class="img-block_left">
-                <img src="images/copy-blocks/stacked_3_mod.jpg" />
+                
             </div>
         </section>
         <section id="block_3" class="content-stripe story-container">
-            <div class="content-block_left">
+            <div class=" ">
                 <h1>full protection</h1>
+                <img src="images/copy-blocks/04.jpg" />
                 <p>
                     Our case offers full phone protection and easy access to buttons and ports.
                 </p>
@@ -68,48 +72,13 @@
                     Our proprietary patented secure slide lock offers a robust yet thin one piece case design for ease of assembly.
                     No more missing pieces or multiple steps to assemble your case. Simply pop in your phone, slide the connector and you’re ready for wireless charging.
                 </p>
-            </div>
-            <div class="img-block_right">
-                <img src="images/copy-blocks/stacked_2.jpg" />
+                
             </div>
         </section>
         <section id="block_4" class="content-stripe story-container">
-            <div class="content-block_right">
-                <h1>why it's better</h1>
-                <p>
-                    Our magnets form a direct contact connection to charge your STACKED power packs and the AC wall plug. 
-                </p>
-
-                <p>
-                    Quickly charge your STACKED power packs with the AC wall plug quick charge.
-                </p>
-            </div>
-            <div class="img-block_left">
-                <img src="images/copy-blocks/stacked_9.jpg" />
-            </div>
+                <img src="images/copy-blocks/05.jpg" />
         </section>
-        <section id="block_5" class="content-stripe story-container">
 
-          <div class="line-block">
-            <img src="images/copy-blocks/stacked_13_a.jpg" />
-              <p class="line-block-descript">
-              Charge your phone
-              </p>
-          </div>
-          <div class="line-block">
-            <img src="images/copy-blocks/stacked_13b.jpg" />
-              <p class="line-block-descript">
-              Charge your power packs
-              </p>
-          </div>
-          <div class="line-block">
-            <img src="images/copy-blocks/stacked_12.jpg" />
-              <p class="line-block-descript">
-              Charge up to 5 power packs
-              </p>
-          </div>
-            
-        </section>
          <section id="mailchimp">
              <?php include("inc/mailchimp.php"); ?>
          </section>
